eacb6bd6f411c028c120ab08cc4c3004da33599a
sagalou <sagal.louise.haider@gmail.com>
2026-05-11 13:06:32 +0200

    fix 0-pascal-triangle.py file with a docstring for the function

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -5,12 +5,12 @@ def pascal_triangle(n):
     """Returns a list of lists representing Pascal's triangle of n"""
     if n <= 0:
         return []
-    triangle = [[1]]
+    pascal_triangle = [[1]]
     for i in range(1, n):
         row = [1]
-        prev = triangle[i - 1]
+        prev = pascal_triangle[i-1]
         for j in range(len(prev) - 1):
-            row.append(prev[j] + prev[j + 1])
+            row.append(prev[j] + prev[j+1])
         row.append(1)
-        triangle.append(row)
-    return triangle
+        pascal_triangle.append(row)
+    return pascal_triangle
