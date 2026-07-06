325766dad5a8328e1a4c723abe1076b0496ea90e
sagalou <sagal.louise.haider@gmail.com>
2026-07-06 14:10:44 +0200

    fix 0-island_perimeter.py file

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -3,6 +3,15 @@
 
 
 def island_perimeter(grid):
+    """Return the perimeter of the island described in grid.
+
+    Args:
+        grid (list): list of lists of integers, where 0 is water
+            and 1 is land.
+
+    Returns:
+        int: perimeter of the island.
+    """
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
